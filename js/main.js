20e647b7608a4e26fb524df5e09dd72889d90e43
Arek <arkinio91@gmail.com>
2021-09-23 18:06:12 +0200

    add popup phone version

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+    //slider
 const slideList = [{
  img: "./assets/2.jpg",
 },
@@ -28,20 +30,62 @@ const changeSlide = () => {
 }
 setInterval(changeSlide, time);
 
-const title = document.querySelector('.article__h2').textContent;
-const description = document.querySelector(".description__p--opis").textContent;
-document.querySelector('.article button').addEventListener("click", function () {
+
+                            //popup
+
+//
+// const btn= document.querySelectorAll(...['.btn']);
+// btn.forEach(elements=>elements.addEventListener('click', ()=>{
+//  console.log('dziala');
+// }))
+
+const titleOne = document.querySelector('.article__h2--one').textContent;
+const titleTwo = document.querySelector('.article__h2--two').textContent;
+const titleThree = document.querySelector('.article__h2--three').textContent;
+const titleFour = document.querySelector('.article__h2--four').textContent;
+const descriptionOne = document.querySelector(".description__p--one").textContent;
+const descriptionTwo = document.querySelector(".description__p--two").textContent;
+const descriptionThree = document.querySelector(".description__p--three").textContent;
+const descriptionFour = document.querySelector(".description__p--four").textContent;
+document.querySelector('.btn__one').addEventListener("click", function () {
  document.querySelector('.modal-wrap').classList.add('active');
  const top = document.querySelector('.top');
  const middle=document.querySelector('.middle');
  top.classList.add('activeTitle');
- top.textContent=title;
- middle.textContent= description;
- 
- 
+ top.textContent=titleOne;
+ middle.textContent= descriptionOne;
 })
 
 
+document.querySelector('.btn__two').addEventListener("click", function () {
+ document.querySelector('.modal-wrap').classList.add('active');
+ const top = document.querySelector('.top');
+ const middle=document.querySelector('.middle');
+ top.classList.add('activeTitle');
+ top.textContent=titleTwo;
+ middle.textContent= descriptionTwo;
+})
+
+
+
+document.querySelector('.btn__three').addEventListener("click", function () {
+ document.querySelector('.modal-wrap').classList.add('active');
+ const top = document.querySelector('.top');
+ const middle=document.querySelector('.middle');
+ top.classList.add('activeTitle');
+ top.textContent=titleThree;
+ middle.textContent= descriptionThree;
+})
+
+
+document.querySelector('.btn__four').addEventListener("click", function () {
+ document.querySelector('.modal-wrap').classList.add('active');
+ const top = document.querySelector('.top');
+ const middle=document.querySelector('.middle');
+ top.classList.add('activeTitle');
+ top.textContent=titleFour;
+ middle.textContent= descriptionFour;
+})
 document.querySelector('span.hide').addEventListener("click", function () {
  document.querySelector('.modal-wrap').classList.remove('active');
 
