7db4595fa7025392718dcd4d8bc7ee59a47b200e
Arek <arkinio91@gmail.com>
2021-09-24 10:25:09 +0200

    finish without course ona 1200px

--- js/main.js
+++ js/main.js
@@ -93,13 +93,6 @@ setInterval(changeSlide, time);
 
 //pobieram wszystkie przyciski
 const btn = document.querySelectorAll(...['.btn']);
-
-
-
-//ustawiam nasłuchiwanie na pojedyńczy przycisk
-btn.forEach(elements=>elements.addEventListener('click', function(e){
-e.preventDefault();
-//pobranie wszystkich textContent
 const titleOne = document.querySelector('.article__h2--one').textContent;
 const titleTwo = document.querySelector('.article__h2--two').textContent;
 const titleThree = document.querySelector('.article__h2--three').textContent;
@@ -108,11 +101,18 @@ const descriptionOne = document.querySelector(".description__p--one").textConten
 const descriptionTwo = document.querySelector(".description__p--two").textContent;
 const descriptionThree = document.querySelector(".description__p--three").textContent;
 const descriptionFour = document.querySelector(".description__p--four").textContent;
+
+
+//ustawiam nasłuchiwanie na pojedyńczy przycisk
+btn.forEach(elements=>elements.addEventListener('click', function(e){
+e.preventDefault();
+//pobranie wszystkich textContent
 document.querySelector('.modal-wrap').classList.add('active');
  const top = document.querySelector('.top');
  const middle=document.querySelector('.middle');
 //pobieram aktualnie kliknięty przycisk
 const show =e.target.name;
+console.log(show);
 
 if(e.target.name==="one"){
     
@@ -144,3 +144,61 @@ document.querySelector('span.hide').addEventListener("click", function () {
      document.querySelector('.modal-wrap').classList.remove('active');
     
     });
+
+
+    //funkcja na article__photo
+
+    const article = document.querySelectorAll(...['.article__photo']);
+    
+    
+
+    article.forEach(elements=>elements.addEventListener('click', function(e){
+        e.preventDefault();
+        //pobranie wszystkich textContent
+        document.querySelector('.modal-wrap').classList.add('active');
+         const top = document.querySelector('.top');
+         const middle=document.querySelector('.middle');
+        //pobieram aktualnie kliknięty przycisk
+        const show =e.target.name;
+        const h2 = document.querySelector('.article__h2');
+        // h2.classList.add('active__h2');
+        // const h2Hover = document.querySelector('.article__h2--hover');
+        // h2Hover.classList.add('active__h2');
+        document.querySelectorAll(...['.article__h2--hover']).forEach(element=>element.style.display="none");  
+        
+        
+        // h2.style.display="none";
+        // console.log(show);
+        top.classList.add('activeTitle');
+        
+        if(e.target.name==="one"){
+            
+            
+        
+         top.textContent=titleOne;
+         middle.textContent= descriptionOne;
+        }
+            else if (e.target.name==="two"){
+                top.textContent=titleTwo;
+         middle.textContent= descriptionTwo;
+                
+            }
+            else if (e.target.name==="three"){
+                
+                top.textContent=titleThree;
+         middle.textContent= descriptionThree;
+            }
+            else if (e.target.name==="four"){
+                top.textContent=titleFour;
+         middle.textContent= descriptionFour;
+            }else{
+                return 0;
+            }
+        
+        
+        }))
+        document.querySelector('span.hide').addEventListener("click", function () {
+             document.querySelector('.modal-wrap').classList.remove('active');
+             document.querySelectorAll(...['.article__h2--hover']).forEach(element=>element.style.display="block"); 
+            
+            });
